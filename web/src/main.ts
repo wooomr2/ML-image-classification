c6f70abb4b5cb3ff659e5a9a02a853089d62a334
wooomr2 <wooomr2@gmail.com>
2024-04-02 10:45:35 +0900

    web server initial setting

---
+++ web/src/main.ts
@@ -0,0 +1,6 @@
+import { SketchPad } from "./sketchPad";
+import "./style.css";
+
+const sketchPadContainer = document.getElementById("sketchPadContainer");
+
+const sketchPad = new SketchPad(sketchPadContainer!);
---
+++ web/src/sketchPad.ts
@@ -0,0 +1,3 @@
+export class SketchPad {
+  constructor(public container: HTMLElement) {}
+}
