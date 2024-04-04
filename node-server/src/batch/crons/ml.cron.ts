35d4f4abb3e08df4171b68fc7ad34d3a91e265a0
wooomr2 <wooomr2@gmail.com>
2024-04-04 11:00:13 +0900

    [node-server] tsconfig 정리

--- node-server/src/batch/crons/ml.cron.ts
+++ node-server/src/batch/crons/ml.cron.ts
@@ -1,3 +1,16 @@
+import { readFileSync } from 'fs'
+import { Point } from 'shared'
+
+const dataDir = '../../../data'
+const constants = {
+  DATA_DIR: dataDir,
+  RAW_DIR: dataDir + '/raw',
+  DATASET_DIR: dataDir + '/dataset',
+  JSON_DIR: dataDir + '/dataset/json',
+  IMG_DIR: dataDir + '/dataset/img',
+  SAMPLES: dataDir + '/dataset/samples.json',
+}
+
 export default class MLCron {
   static async run() {
     console.log('RUN MLCron')
@@ -6,5 +19,21 @@ export default class MLCron {
 
   static async _generate_dataset() {
     console.log('GENERATING DATASET ...')
+
+    const fileNames = readFileSync(constants.RAW_DIR, { encoding: 'utf-8' })
+    const samples = []
+    let id = 1
+
+    for (const fileName of fileNames) {
+      const content = readFileSync(constants.RAW_DIR + '/' + fileName, { encoding: 'utf-8' })
+
+      interface IContent {
+        session: number
+        student: string
+        drawings: Record<string, Point[][]>
+      }
+
+      const { session, student, drawings } = JSON.parse(content)
+    }
   }
 }
