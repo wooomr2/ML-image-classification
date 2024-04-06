731517e2ad2fd83437fbb260ff9dedb0fa7cba1e
wooomr2 <wooomr2@gmail.com>
2024-04-06 12:26:31 +0900

    [shared] groupBy util & sample 타입 추가

--- shared/src/types.ts
+++ shared/src/types.ts
@@ -1,5 +1,5 @@
-import { Point } from './primitives/point'
 import { IMAGE_LABELS } from './const'
+import { Point } from './primitives/point'
 
 export type TLabel = (typeof IMAGE_LABELS)[number]
 
@@ -15,4 +15,12 @@ export interface IPreProcessedData {
   session: number
   student: string
   drawings: Record<TLabel, Path[]>
+}
+
+export interface ISample {
+  id: number
+  label: string
+  student_id: number
+  student_name: string
+  paths: Path[]
 }
--- shared/src/util.ts
+++ shared/src/util.ts
@@ -6,4 +6,20 @@ export class Util {
   static formatPercent(n: number, digit: number = 2): string {
     return Util.formatNumber(n, digit) + '%'
   }
+
+  static groupBy<T>(arr: T[], prop: keyof T): Record<string, T[]> {
+    const groups = arr.reduce(
+      (acc, obj) => {
+        const key = String(obj[prop])
+        if (!acc[key]) {
+          acc[key] = []
+        }
+        acc[key].push(obj)
+        return acc
+      },
+      {} as Record<string, T[]>
+    )
+
+    return groups
+  }
 }
