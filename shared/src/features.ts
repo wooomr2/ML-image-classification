04cfc69a7b375c7b9042b59f1d43824260bfd7a2
wooomr2 <wooomr2@gmail.com>
2024-04-06 14:17:59 +0900

    [shared] features 추가

---
+++ shared/src/features.ts
@@ -0,0 +1,10 @@
+import { Path } from './types'
+
+export function getPathCount(paths: Path[]) {
+  return paths.length
+}
+
+export function getPointCount(paths: Path[]) {
+  const points = paths.flat()
+  return points.length
+}
